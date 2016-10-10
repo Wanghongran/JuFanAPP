3effcf1030db0ac22f6ed7395534a4df33402058
name <m18801273650@163.com>
2016-10-10 10:05:58 +0800

    王红冉提交

--- jufan/app/src/main/java/com/example/wanghongra/jufan/GongchangActivity.java
+++ jufan/app/src/main/java/com/example/wanghongra/jufan/GongchangActivity.java
@@ -3,11 +3,11 @@ package com.example.wanghongra.jufan;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends Activity {
+public class GongchangActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_gongchang);
     }
 }
